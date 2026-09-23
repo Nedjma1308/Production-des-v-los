6383d6966fa7eae2dfb2b9f72ac0470fed1a2330
Nedjma1308 <nedjmabenamar13@gmail.com>
2026-09-23 10:52:45 +0200

    Add abstract model QuantiteProduit to models.py

--- velo/high_level/models.py
+++ velo/high_level/models.py
@@ -1,1 +1,10 @@
 # Create your models here.
+class QuantiteProduit(models.Model):
+ quantite = models.IntegerField()
+ matiere_premiere = models.ForeignKey(
+ Produit,
+ on_delete=models.PROTECT,
+ )
+ class Meta:
+ abstract = True
+
